17ef8af14751f9fe83881ac0222f1ea9f827f4bd
sjss17647 <sjss17647@gmail.com>
2022-04-30 14:44:02 +0500

    refactoring

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,21 +2,16 @@ import { ThemeProvider } from '@emotion/react'
 import Layout from 'components/Layout'
 import 'styles/globals.scss'
 import theme from 'mui-theme'
-import { QueryClient, QueryClientProvider } from 'react-query'
 import { useState } from 'react'
-// import { ReactQueryDevtools } from 'react-query/devtools'
 
 function MyApp({ Component, pageProps }) {
-  const [queryClient] = useState(() => new QueryClient())
   return (
-    <QueryClientProvider client={queryClient}>
-      <ThemeProvider theme={theme}>
-        <Layout>
-          <Component {...pageProps} />
-          {/* <ReactQueryDevtools initialIsOpen={false} /> */}
-        </Layout>
-      </ThemeProvider>
-    </QueryClientProvider>
+    <ThemeProvider theme={theme}>
+      <Layout>
+        <Component {...pageProps} />
+        {/* <ReactQueryDevtools initialIsOpen={false} /> */}
+      </Layout>
+    </ThemeProvider>
   )
 }
 

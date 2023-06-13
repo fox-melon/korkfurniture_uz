cdded88429ff7641023fddd83ae210dc1ec53601
Munirabonu <muniratillaxojayeva@gmail.com>
2023-06-13 20:04:30 +0500

    feat: about component

---
+++ src/components/UI/About/index.jsx
@@ -0,0 +1,43 @@
+import { Container } from "@mui/material";
+import useTranslation from "next-translate/useTranslation";
+import styles from "./style.module.scss";
+import Image from "next/image";
+
+export default function About() {
+  const { t } = useTranslation("about");
+  return (
+    <Container>
+      <div className={styles.about}>
+        <h1 className={styles.heading}>Biz haqimizda</h1>
+        <div className={styles.block}>
+          <div className={styles.textWrap}>
+            <h2 className={styles.title}>
+              Avtomatizatsiya “БАЗИС МЕБЕЛЬЩИК” <br /> Biz bilan!
+            </h2>
+            <p className={styles.text}>
+              Mebel ishlab chiqarishdagi eng muhim jarayon, mebel kontruksiyasi
+              qilishdir. Ushbu jarayon, aynan mebel konstrusiyalarini amalga
+              oshirishga mo’ljallangan Bazis dasturi yordamida qilinadi. Bu esa
+              mahsulotlarimiz sifatiga katta ta’sir o’tkazadi
+            </p>
+          </div>
+          <div className={styles.imgWrap}>
+            <Image
+              src={"/images/aboutImg.svg"}
+              priority={true}
+              alt="БАЗИС МЕБЕЛЬЩИК"
+              width={636}
+              height={356}
+              layout="fixed"
+            />
+          </div>
+        </div>
+        <h1 className={styles.heading}>Kompaniya maqsadi</h1>
+        <h2 className={styles.desc}>
+           Mebel ishlab chiqarish orqali insonlarga yaxshilik qilish va  <br /> ularga
+          halovat ulashish! 
+        </h2>
+      </div>
+    </Container>
+  );
+}

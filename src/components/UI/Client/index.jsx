de689d4b4d26e9d08205bfa090788601974b4b3f
Munirabonu <muniratillaxojayeva@gmail.com>
2023-07-01 15:40:04 +0500

    fix:clients logo

--- src/components/UI/Client/index.jsx
+++ src/components/UI/Client/index.jsx
@@ -2,35 +2,44 @@ import { Container } from "@mui/material";
 import useTranslation from "next-translate/useTranslation";
 import styles from "./style.module.scss";
 import Image from "next/image";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Skeleton from "@mui/material/Skeleton";
 
 export default function Client() {
+  const [data, setData] = useState();
   const { t } = useTranslation("common");
-  const data = [
-    { id: 1, img: "/images/natGraphLogo.svg" },
-    { id: 2, img: "/images/discLogo.svg" },
-    { id: 3, img: "/images/ikealogo.svg" },
-    { id: 4, img: "/images/herMilLogo.svg" },
-    { id: 5, img: "/images/woodyLogo.svg" },
-    { id: 6, img: "/images/woodyWorkLogo.svg" },
-    { id: 7, img: "/images/okoLogo.svg" },
-    { id: 8, img: "/images/SteelLogo.svg" },
-  ];
+
+  useEffect(() => {
+    axios
+      .get(`https://api.quickweb.uz/adfbddee-4ecb-4767-973c-fd3f16bd6ebd`)
+      .then((res) => setData(res?.data));
+  }, []);
+
+  
   return (
     <Container id="client">
       <h1 className={styles.heading}>{t("our_clients")}</h1>
       <div className={styles.logoWrap}>
-        {data.map((item) => {
-          return (
+        {data ? (
+          data?.data.map((item, index) => (
             <Image
-              key={item.id}
-              src={item.img}
+              key={index}
+              src={item.logo}
               alt="brand"
               width={372}
               height={152}
               layout="responsive"
             />
-          );
-        })}
+          ))
+        ) : (
+          <>
+            <Skeleton variant="rounded" width={"100%"} height={152} />
+            <Skeleton variant="rounded" width={"100%"} height={152} />
+            <Skeleton variant="rounded" width={"100%"} height={152} />
+            <Skeleton variant="rounded" width={"100%"} height={152} />
+          </>
+        )}
       </div>
     </Container>
   );

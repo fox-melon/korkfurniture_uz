0c731f9b643852072e65bd7c7bce137ea7ac5c28
Munirabonu <muniratillaxojayeva@gmail.com>
2023-07-05 21:37:45 +0500

    fix: eslint error i18n defaultLocale

--- i18n.js
+++ i18n.js
@@ -1,5 +1,6 @@
 module.exports = {
   locales: ['ru', 'en', 'uz'],
+  defaultLocale: 'uz',
   loadLocaleFrom: (lang, ns) =>
     import(`@/locales/${lang}/common.json`).then((m) => m.default),
   pages: {
